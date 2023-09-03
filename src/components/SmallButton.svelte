7bb17f70fdab9fa3056b6ddaecd5036733043d43
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2023-09-03 17:37:38 +0530

    new options to remove and add

---
+++ src/components/SmallButton.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let text: string;
+	export let onClick: () => void;
+	export let dashed = false;
+</script>
+
+<button class={`${dashed && 'dashed'}`} on:click={onClick}>{text}</button>
+
+<style>
+	button {
+		aspect-ratio: 1;
+		height: 4rem;
+		background: var(--color-black);
+		border: 2px solid var(--color-gray);
+		border-radius: 0.5rem;
+		font-family: 'Poppins';
+		color: var(--color-white);
+		text-align: center;
+	}
+
+	.dashed {
+		border: 2px dashed var(--color-gray);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,53 @@
 <script lang="ts">
 	import { choices } from '$stores/choices';
+	import SmallButton from '../components/SmallButton.svelte';
 	let chooseRandom = false;
+
+	function removeChoice(index: any) {
+		choices.remove(index);
+		console.log('Hello');
+	}
+
+	let addValue: string;
+
+	function addChoice() {
+		if (addValue != '') {
+			choices.add(addValue);
+			addValue = '';
+		}
+	}
 </script>
 
 <div class="container">
 	{#if !chooseRandom}
 		<p>{$choices}</p>
-		{#each $choices as choice, id (id)}
-			<input type="text" bind:value={choice} />
+		{#each Object.entries($choices) as [index, choice] (index)}
+			<div class="choice">
+				<input type="text" bind:value={choice} />
+				<SmallButton
+					text="x"
+					onClick={() => {
+						removeChoice(index);
+					}}
+				/>
+			</div>
 		{/each}
-		<button class="add" on:click={() => choices.add('')}> + Choice </button>
+		<div class="choice">
+			<input
+				class="add"
+				type="text"
+				bind:value={addValue}
+				placeholder="Ex: Heads"
+				on:change={addChoice}
+			/>
+			<SmallButton text="+" onClick={addChoice} dashed />
+		</div>
 	{:else}
 		<p>The chosen one is</p>
 		<h1>{$choices[Math.floor(Math.random() * $choices.length)]}</h1>
 	{/if}
 
-	<button on:click={() => (chooseRandom = !chooseRandom)}>PICK</button>
+	<button class="cta" on:click={() => (chooseRandom = !chooseRandom)}>PICK</button>
 </div>
 
 <style>
@@ -33,16 +65,47 @@
 		background-color: var(--color-black);
 		color: var(--color-white);
 		border-radius: 0.5rem;
-		height: 3rem;
-		width: 32rem;
-		border: 1px solid var(--color-gray);
-		padding: 0 8px;
+		height: 4rem;
+		width: 28rem;
+		border: 2px solid var(--color-gray);
 		font-family: 'Poppins';
 		text-align: center;
+		box-sizing: border-box;
+		-moz-box-sizing: border-box;
+		-webkit-box-sizing: border-box;
 	}
 
 	input:focus {
 		background-color: var(--color-gray);
-		outline: 1px solid var(--color-gray);
+		outline: 2px solid var(--color-gray);
+	}
+
+	.choice {
+		display: flex;
+		gap: 0.5rem;
+		align-items: center;
+	}
+
+	.add {
+		border: 2px dashed var(--color-gray);
+	}
+
+	.cta {
+		position: fixed;
+		width: 10rem;
+		height: 4rem;
+		z-index: 10;
+		bottom: 0;
+		margin: 4rem 0;
+		border: 0;
+		border-radius: 4rem;
+		background: var(--color-white);
+		font-family: Poppins;
+		font-size: 1.25rem;
+		font-style: normal;
+		font-weight: 600;
+		line-height: normal;
+		text-transform: uppercase;
+		color: var(--color-black);
 	}
 </style>
